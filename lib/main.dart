fbd55254ef3226a8831f6a7ebc80bb6da82d80e3
Ahmed Farouk <ahmedmohamedfarouk212103048@gmail.com>
2025-03-04 16:39:52 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SecurityApp());
+}
+
+class SecurityApp extends StatelessWidget {
+  const SecurityApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
